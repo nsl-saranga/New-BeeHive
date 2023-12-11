9829d8ceb23ec9428ffa0a7fbffa1df0b8e3dd57
nsl-web <finn.collins129@gmail.com>
2023-12-11 17:40:32 +0530

    added the code for HX711

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
+#include <HX711.h>
 
 #define DHT22PIN 2
 #define DHTTYPE DHT22
-DHT dht(DHT22PIN, DHTTYPE); 
+// HX711 circuit wiring
+#define LOADCELL_DOUT_PIN 3 
+#define LOADCELL_SCK_PIN 4  
+#define calibration_factor -7050.0 
+DHT dht(DHT22PIN, DHTTYPE);
+HX711 scale; 
 
-int chk;
 float humidity;
-float temp;  
+float temp; 
+float weight; 
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  dht.begin(); 
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 }
 
 void loop() {
@@ -25,15 +33,8 @@ void loop() {
   Serial.print(" Celsius, Humidity: ");
   Serial.print(humidity);
   Serial.println(" %");
-}
 
-
-// put function declarations here:
-int myFunction(int, int);
-
-
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
+  Serial.print(" kgs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.println();
 }
